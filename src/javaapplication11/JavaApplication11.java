f7a7d701fc43044d67ac921e51babc61fce5ac0d
Atteneri Maria Rodriguez Sanchez <attrodsan@alu.edu.gva.es>
2024-01-19 12:47:17 +0100

    Segundo commit JavaApplication11. Creo código con error.

--- src/javaapplication11/JavaApplication11.java
+++ src/javaapplication11/JavaApplication11.java
@@ -14,7 +14,7 @@ public class JavaApplication11 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Esto es una simulación de examen.")
     }
     
 }
